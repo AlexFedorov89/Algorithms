d248fa7fed17753829c83cf7a0deab0c1e29c21a
Alex <alex.fedorov1989@yandex.ru>
2019-06-24 14:06:40 +0200

    Add negative exponent.

--- lesson4/Ex1.java
+++ lesson4/Ex1.java
@@ -2,18 +2,22 @@ package lesson4;
 
 public class Ex1 {
     public static void main(String[] args) {
-        System.out.println(exponentiation(3,3));
+        System.out.println(exponentiation(3, 3));
     }
 
-    public static int exponentiation(int value, int degree){
+    public static double exponentiation(double value, int degree) {
         return exponentiation(value, degree, 1);
     }
 
-    public static int exponentiation(int value, int degree, int count){
-        if (degree == count){
+    public static double exponentiation(double value, int degree, int count) {
+        if (Math.abs(degree) == (double) count) {
             return value;
         }
-
-        return value * exponentiation(value, degree, ++count);
+        
+        if (degree < 0 && count == 1) {
+            return 1 / (value * exponentiation(value, degree, ++count));
+        } else {
+            return value * exponentiation(value, degree, ++count);
+        }
     }
 }
