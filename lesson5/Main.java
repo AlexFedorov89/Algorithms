d1286eb9eeeab689f457719753c9aa9f71e45e57
Alex <alex.fedorov1989@yandex.ru>
2019-06-30 14:52:50 +0300

    Add msg of balanced tree.

--- lesson5/Main.java
+++ lesson5/Main.java
@@ -1,25 +1,29 @@
 package lesson5;
 
 
-
 public class Main {
 
     static final int MAX_LEVEL = 4;
+    static final int TREE_COUNT = 20;
 
     public static void main(String[] args) {
 
         TreeImpl<Integer>[] arrayTree = new TreeImpl[20];
+        int balancedTreeCount = 0;
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < TREE_COUNT; i++) {
             arrayTree[i] = new TreeImpl<>();
 
-            while (arrayTree[i].getCurrentLevel() < MAX_LEVEL){
+            while (arrayTree[i].getCurrentLevel() < MAX_LEVEL) {
                 arrayTree[i].add((int) Math.round((Math.random() * 40) - 20));
+            }
+
+            if (arrayTree[i].isBalanced()) {
+                balancedTreeCount++;
             }
         }
 
-        arrayTree[2].display();
-
+        System.out.println("Balanced tree count: " + (balancedTreeCount / (TREE_COUNT * 1.0) * 100) + "%");
 
 
     }
